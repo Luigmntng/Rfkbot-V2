0e545badff8db93030392d787bf238653c751989
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-18 15:44:26 +0700

    Update hentaigif.js

--- plugins/hentaigif.js
+++ plugins/hentaigif.js
@@ -1,15 +1,25 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, usedPrefix, command }) => {
-	if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-   heum = await fetch(`https://restapi-production-a62b.up.railway.app/api/nsfw/gifs?apikey=APIKEY`)
-   if (!heum) throw json
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '© AhmdLui', 'GET DOSA', `${usedPrefix + command}`, m)
+let fetch = require("node-fetch")
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn}) => {
+m.reply(wait)
+if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
+  try {
+  let res = await fetch(`https://restapi-production-a62b.up.railway.app/api/nsfw/gifs?apikey=APIKEY`)
+  let json = await res.json()
+  let { 
+result
+} = json
+let stiker = await sticker(null, result, 'Kiss', 'Bot')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+ } catch (e) {
+  }
 }
 handler.help = ['hentaigif']
 handler.tags = ['hentai']
-
-handler.command = /^(hentaigif)$/i
-handler.limit = true
+handler.command = /^hentaigif$/i
 
 module.exports = handler
