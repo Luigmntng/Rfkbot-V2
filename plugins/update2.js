877bb331e3662a6569ac3e3c7966127d0e46ba8f
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-18 10:54:46 +0700

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'Luigmntng/ShiraoriBOT-Md'
+let repository = 'Luigmntng/Rfkbot-V2'
 let branch = 'master'
 
 async function handler(m, { text }) {
